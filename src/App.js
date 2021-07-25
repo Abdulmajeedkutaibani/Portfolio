83e653358024d469c59293ae1fca21b86e4c6f39
Abdulmajeed Kutaibani <Abodymeo@yahoo.com>
2021-07-26 01:14:31 +0300

    create about component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import About from './components/About';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
 function App() {
-  return <div className='App'></div>;
+  return (
+    <main className='text-gray-400 bg-gray-900 body-font'>
+      <Navbar />
+      <About />
+      <Projects />
+      <Contact />
+    </main>
+  );
 }
 
 export default App;
