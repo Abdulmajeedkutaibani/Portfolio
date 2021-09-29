25241221e7e463b8303725fa0c72b45ac3d49f78
Abdulmajeed Kutaibani <Abodymeo@yahoo.com>
2021-09-29 03:42:15 +0300

    add bootstrap to skills

--- src/data.js
+++ src/data.js
@@ -3,13 +3,13 @@ export const projects = [
     title: 'AKAudio',
     subtitle: 'React.js / Tailwind CSS / React-hooks / React-router',
     description: `AKAudio is an all-in-one stop to fulfill your audio needs.
-    
+
 This website has been my biggest project so far and the most challenging. I had to solve many problems that I had'nt encountered before and also needed to pay much attention to the smallest of details.
 
 I feel that after building this website my confidence in my coding ability and in my ability to learn and apply has skyrocketed! 🚀 😃
 
 check it out! and any feedback is much appreciated. 😁 👍🏼`,
-    image: './E-commerce.webp',
+    image: './AKAudio-preview.jpg',
     link: 'https://akaudio.netlify.app/',
   },
   {
@@ -61,6 +61,7 @@ export const skills = [
   'HTML/CSS',
   'SASS',
   'Tailwind CSS Framework',
+  'Bootstrap 5',
   'JavaScript',
   'React.js',
   'Git',
